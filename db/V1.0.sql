81865ad022c1cfea4035d6bda4fd1f5e4caada42
timber <eskyzdt@sina.com>
2019-11-17 21:49:16 +0800

    feat(*): 增加物品

--- db/V1.0.sql
+++ db/V1.0.sql
@@ -22,5 +22,45 @@ CREATE TABLE `item` (
 	`ap` BIGINT ( 20 ) UNSIGNED NOT NULL DEFAULT '0' COMMENT '魔攻',
 	`ad_def` BIGINT ( 20 ) UNSIGNED NOT NULL DEFAULT '0' COMMENT '物防',
 	`ap_def` BIGINT ( 20 ) UNSIGNED NOT NULL DEFAULT '0' COMMENT '魔防',
+	`speed` BIGINT ( 20 ) UNSIGNED NOT NULL DEFAULT '0' COMMENT '速度',
 PRIMARY KEY ( `id` )
 ) ENGINE = INNODB DEFAULT CHARSET = utf8mb4 ROW_FORMAT = DYNAMIC COMMENT = '物品表';
+
+
+-- 导入物品表
+INSERT INTO `item`(`id`, `item_name`, `serial`, `item_type`, `item_price`, `ad`, `ap`, `ad_def`, `ap_def`, `speed`) VALUES (1, '许愿星', 0, 1, 580, 0, 5000, 0, 0, 1000);
+INSERT INTO `item`(`id`, `item_name`, `serial`, `item_type`, `item_price`, `ad`, `ap`, `ad_def`, `ap_def`, `speed`) VALUES (2, '狮子心', 0, 1, 580, 5000, 0, 0, 0, 1000);
+INSERT INTO `item`(`id`, `item_name`, `serial`, `item_type`, `item_price`, `ad`, `ap`, `ad_def`, `ap_def`, `speed`) VALUES (3, '守护之杖', 0, 1, 2480, 20000, 0, 0, 0, 3000);
+INSERT INTO `item`(`id`, `item_name`, `serial`, `item_type`, `item_price`, `ad`, `ap`, `ad_def`, `ap_def`, `speed`) VALUES (4, '创世兵器', 0, 1, 2480, 16000, 0, 0, 0, 5000);
+INSERT INTO `item`(`id`, `item_name`, `serial`, `item_type`, `item_price`, `ad`, `ap`, `ad_def`, `ap_def`, `speed`) VALUES (5, '守护神杖', 0, 1, 7960, 0, 40000, 0, 0, 6000);
+INSERT INTO `item`(`id`, `item_name`, `serial`, `item_type`, `item_price`, `ad`, `ap`, `ad_def`, `ap_def`, `speed`) VALUES (6, '创世神器', 0, 1, 7960, 30000, 0, 0, 0, 12000);
+INSERT INTO `item`(`id`, `item_name`, `serial`, `item_type`, `item_price`, `ad`, `ap`, `ad_def`, `ap_def`, `speed`) VALUES (7, '宿命之剑', 0, 1, 8980, 12000, 0, 0, 0, 0);
+INSERT INTO `item`(`id`, `item_name`, `serial`, `item_type`, `item_price`, `ad`, `ap`, `ad_def`, `ap_def`, `speed`) VALUES (8, '宿命之杖', 0, 1, 8980, 0, 12000, 0, 0, 0);
+INSERT INTO `item`(`id`, `item_name`, `serial`, `item_type`, `item_price`, `ad`, `ap`, `ad_def`, `ap_def`, `speed`) VALUES (9, '宿命之枪', 0, 1, 8980, 8000, 8000, 1000, 1000, 6000);
+INSERT INTO `item`(`id`, `item_name`, `serial`, `item_type`, `item_price`, `ad`, `ap`, `ad_def`, `ap_def`, `speed`) VALUES (10, '正宗 空', 0, 1, 21980, 20000, 20000, 2000, 2000, 15000);
+INSERT INTO `item`(`id`, `item_name`, `serial`, `item_type`, `item_price`, `ad`, `ap`, `ad_def`, `ap_def`, `speed`) VALUES (11, '生者必灭', 0, 1, 44980, 30000, 30000, 10000, 10000, 20000);
+INSERT INTO `item`(`id`, `item_name`, `serial`, `item_type`, `item_price`, `ad`, `ap`, `ad_def`, `ap_def`, `speed`) VALUES (12, '女神头盔', 0, 2, 480, 800, 800, 800, 800, 0);
+INSERT INTO `item`(`id`, `item_name`, `serial`, `item_type`, `item_price`, `ad`, `ap`, `ad_def`, `ap_def`, `speed`) VALUES (13, '光之眼改', 0, 2, 2480, 2500, 2500, 3000, 3000, 1000);
+INSERT INTO `item`(`id`, `item_name`, `serial`, `item_type`, `item_price`, `ad`, `ap`, `ad_def`, `ap_def`, `speed`) VALUES (14, '暗之眼改', 0, 2, 7480, 6000, 6000, 6000, 6000, 3000);
+INSERT INTO `item`(`id`, `item_name`, `serial`, `item_type`, `item_price`, `ad`, `ap`, `ad_def`, `ap_def`, `speed`) VALUES (15, '女神之铠', 0, 3, 480, 700, 700, 1200, 1200, 400);
+INSERT INTO `item`(`id`, `item_name`, `serial`, `item_type`, `item_price`, `ad`, `ap`, `ad_def`, `ap_def`, `speed`) VALUES (16, '光之铠改', 0, 3, 2480, 2000, 2000, 4500, 4500, 700);
+INSERT INTO `item`(`id`, `item_name`, `serial`, `item_type`, `item_price`, `ad`, `ap`, `ad_def`, `ap_def`, `speed`) VALUES (17, '暗之铠改', 0, 3, 7480, 6000, 6000, 9999, 9999, 1500);
+INSERT INTO `item`(`id`, `item_name`, `serial`, `item_type`, `item_price`, `ad`, `ap`, `ad_def`, `ap_def`, `speed`) VALUES (18, '破坏神铠改', 0, 3, 11980, 9999, 9999, 9999, 9999, 0);
+INSERT INTO `item`(`id`, `item_name`, `serial`, `item_type`, `item_price`, `ad`, `ap`, `ad_def`, `ap_def`, `speed`) VALUES (19, '救世主铠改', 0, 3, 11980, 9999, 9999, 9999, 9999, 0);
+INSERT INTO `item`(`id`, `item_name`, `serial`, `item_type`, `item_price`, `ad`, `ap`, `ad_def`, `ap_def`, `speed`) VALUES (20, '女神护手', 0, 4, 480, 1200, 1200, 400, 400, 300);
+INSERT INTO `item`(`id`, `item_name`, `serial`, `item_type`, `item_price`, `ad`, `ap`, `ad_def`, `ap_def`, `speed`) VALUES (21, '光之痕改', 0, 4, 2480, 4500, 4500, 1500, 1500, 1000);
+INSERT INTO `item`(`id`, `item_name`, `serial`, `item_type`, `item_price`, `ad`, `ap`, `ad_def`, `ap_def`, `speed`) VALUES (22, '暗之痕改', 0, 4, 7480, 9999, 9999, 4000, 4000, 3000);
+INSERT INTO `item`(`id`, `item_name`, `serial`, `item_type`, `item_price`, `ad`, `ap`, `ad_def`, `ap_def`, `speed`) VALUES (23, '女神之靴', 0, 5, 480, 600, 600, 400, 400, 1000);
+INSERT INTO `item`(`id`, `item_name`, `serial`, `item_type`, `item_price`, `ad`, `ap`, `ad_def`, `ap_def`, `speed`) VALUES (24, '光之翼改', 0, 5, 2480, 1500, 1500, 2000, 2000, 3500);
+INSERT INTO `item`(`id`, `item_name`, `serial`, `item_type`, `item_price`, `ad`, `ap`, `ad_def`, `ap_def`, `speed`) VALUES (25, '暗之翼改', 0, 5, 7480, 4000, 4000, 4000, 4000, 9000);
+INSERT INTO `item`(`id`, `item_name`, `serial`, `item_type`, `item_price`, `ad`, `ap`, `ad_def`, `ap_def`, `speed`) VALUES (26, '荣誉戒指', 0, 9, 180, 0, 0, 1030, 1030, 10);
+INSERT INTO `item`(`id`, `item_name`, `serial`, `item_type`, `item_price`, `ad`, `ap`, `ad_def`, `ap_def`, `speed`) VALUES (27, '真灭亡之歌', 0, 9, 4980, 0, 9999, 999, 999, 999);
+INSERT INTO `item`(`id`, `item_name`, `serial`, `item_type`, `item_price`, `ad`, `ap`, `ad_def`, `ap_def`, `speed`) VALUES (28, '真新月之舞', 0, 9, 4980, 9999, 0, 999, 999, 999);
+INSERT INTO `item`(`id`, `item_name`, `serial`, `item_type`, `item_price`, `ad`, `ap`, `ad_def`, `ap_def`, `speed`) VALUES (29, '真龙之卷', 0, 9, 11980, 0, 15000, 0, 5000, 0);
+INSERT INTO `item`(`id`, `item_name`, `serial`, `item_type`, `item_price`, `ad`, `ap`, `ad_def`, `ap_def`, `speed`) VALUES (30, '真虎之卷', 0, 9, 11980, 15000, 0, 5000, 0, 0);
+INSERT INTO `item`(`id`, `item_name`, `serial`, `item_type`, `item_price`, `ad`, `ap`, `ad_def`, `ap_def`, `speed`) VALUES (31, '龙腾四海', 0, 9, 19980, 0, 30000, 0, 0, 10000);
+INSERT INTO `item`(`id`, `item_name`, `serial`, `item_type`, `item_price`, `ad`, `ap`, `ad_def`, `ap_def`, `speed`) VALUES (32, '凤舞九天', 0, 9, 19980, 20000, 0, 0, 0, 20000);
+INSERT INTO `item`(`id`, `item_name`, `serial`, `item_type`, `item_price`, `ad`, `ap`, `ad_def`, `ap_def`, `speed`) VALUES (33, '真蝎狮王', 0, 8, 4980, 5000, 5000, 5000, 5000, 2500);
+INSERT INTO `item`(`id`, `item_name`, `serial`, `item_type`, `item_price`, `ad`, `ap`, `ad_def`, `ap_def`, `speed`) VALUES (34, '真蓝龙', 0, 8, 9980, 10000, 10000, 7500, 7500, 3000);
+INSERT INTO `item`(`id`, `item_name`, `serial`, `item_type`, `item_price`, `ad`, `ap`, `ad_def`, `ap_def`, `speed`) VALUES (35, '真帝龙', 0, 8, 17980, 20000, 20000, 10000, 10000, 5000);
+INSERT INTO `item`(`id`, `item_name`, `serial`, `item_type`, `item_price`, `ad`, `ap`, `ad_def`, `ap_def`, `speed`) VALUES (36, '水晶圣龙', 0, 8, 39980, 40000, 40000, 20000, 20000, 10000);
