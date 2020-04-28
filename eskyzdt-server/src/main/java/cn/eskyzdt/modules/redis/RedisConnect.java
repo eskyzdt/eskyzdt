6ce6d79d5d7cf55954e0a5a8c7e9aabde16b724a
timber <eskyzdt@sina.com>
2020-04-28 11:26:29 +0800

    feat(*): redis连接带密码

--- eskyzdt-server/src/main/java/cn/eskyzdt/modules/redis/RedisConnect.java
+++ eskyzdt-server/src/main/java/cn/eskyzdt/modules/redis/RedisConnect.java
@@ -5,7 +5,9 @@ import redis.clients.jedis.Jedis;
 public class RedisConnect {
 
     public static void main(String[] args) {
-        Jedis jedis = new Jedis("49.235.24.173",6379);
+        Jedis jedis = new Jedis("49.235.24.173",6380);
+        // p密码不对会报redis.clients.jedis.exceptions.JedisDataException: ERR invalid passwordredis.clients.jedis.exceptions.JedisDataException: ERR invalid password
+        jedis.auth("eskyzdt63796379");
         jedis.set("aaa","ccc");
         jedis.close();
 
@@ -14,7 +16,8 @@ public class RedisConnect {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        jedis = new Jedis("49.235.24.173",6379);
+        jedis = new Jedis("49.235.24.173",6380);
+        jedis.auth("eskyzdt63796379");
         String aaa = jedis.get("aaa");
         System.out.println(aaa);
         jedis.close();
--- eskyzdt-server/src/main/java/cn/eskyzdt/modules/redis/RedisPool.java
+++ eskyzdt-server/src/main/java/cn/eskyzdt/modules/redis/RedisPool.java
@@ -7,42 +7,38 @@ import com.rabbitmq.client.UnblockedCallback;
 import redis.clients.jedis.Jedis;
 
 import java.util.*;
-import java.util.function.Consumer;
+import java.util.concurrent.Callable;
 
 public class RedisPool {
 
     private static Map<String, Object> pool = new HashMap<>();
 
-/*
-    public static Jedis getConnector (String param) {
+    public static Jedis getConnector(String param) {
         if (pool.containsKey(param)) {
             return (Jedis) pool.get(param);
         } else {
-            Jedis jedis = new Jedis();
+            Jedis jedis = new Jedis("49.235.24.173",6380);
+            jedis.auth("eskyzdt63796379");
             pool.put(param, jedis);
             return jedis;
         }
     }
-*/
-    private void getStr() {
-        System.out.println("sdfsdfsdfsdf");
+
+    public static Jedis getConnector() {
+        if (!pool.containsKey("")) {
+            Jedis jedis = new Jedis("49.235.24.173",6380);
+            jedis.auth("eskyzdt63796379");
+            pool.put("", jedis);
+        }
+        return (Jedis) pool.get("");
     }
 
-
-    public static void main(String[ ] args) {
-        ArrayList<String> strings = new ArrayList<>();
-        strings.add("1");
-        strings.add(1,"1");
-        strings.add(2,"1");
-
-        RedisPool redisPool = new RedisPool();
-        FunctionalTest getStr = redisPool::getStr;
-        getStr.Th1();
-
-        // strings.forEach(System.out::println);
-
-
-
+    public static void main(String[] args) throws Exception {
+        Callable functionalTest = RedisPool::getConnector;
+        Jedis call = (Jedis) functionalTest.call();
+        String aaa = call.get("aaa");
+        System.out.println(aaa);
+        call.close();
     }
 
 }
